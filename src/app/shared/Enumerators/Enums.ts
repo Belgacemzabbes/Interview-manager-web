bf9f943fb576c791cfdb8ddc9fad44befedc5d57
belgacemzabbes <belgacemstic@gmail.com>
2022-08-19 15:33:15 +0100

    print

--- src/app/shared/Enumerators/Enums.ts
+++ src/app/shared/Enumerators/Enums.ts
@@ -9,16 +9,21 @@ export enum PageSizeEnum {
   Page75 = '75',
   Page100 = '100',
 }
-export enum EtatEntretienENum{
-    EnCours = "En cours",
-    Presence = "Présent",
-    Preinscrit = "Pré-inscrit",
-    Paiement = "Paiement",
-    Attestation = "Attestation",
-    Refuse = "Refusé"
+export enum EtatEntretienENum {
+  EnCours = 'En cours',
+  Presence = 'Présent',
+  Preinscrit = 'Pré-inscrit',
+  Paiement = 'Paiement',
+  Attestation = 'Attestation',
+  Refuse = 'Refusé',
 }
 
 export const PageSizeEnumList: {
   key: string;
   value: string;
 }[] = Object.entries(PageSizeEnum).map(([key, value]) => ({ key, value }));
+
+export enum ReportTypeEnum {
+  PDF = 'pdf',
+  WORD = 'word',
+}
